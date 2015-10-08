857d15864fecbaeae542c9a90ac3514cec90e1b1
txemagon / imasen <txema.gonz@gmail.com>
2015-10-09 00:16:29 +0200

    remote_install

--- remote_dop.sh
+++ remote_dop.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # remote_dop.sh
 
+# Oct, 2015
+# txema.gonz@gmail.com
+
+# Executes simultanousley several processes 
+# (ONE WINDOW EACH) in background.
+# Every command supplied feeds remote input, so there aren't
+# any interacting possibilities (no remote configure).
+
+# Usage: ./remote_dop.sh <command_file>
+
+# Note: Point with userhost.lst (symbolic link) to a file
+# containing the hosts
+
 terminal="gnome-terminal"
 option=" --tab -e "
 s="'"
