02bb99088c2a71810f50ce26e75c68492da6b874
txemagon <txema.gonz@gmail.com>
2015-10-15 12:39:01 +0200

    push config files to remote dir

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# push.sh
-
-# Usage:
-
-for ip in `cat userhost.lst`; do
-   scp repository/usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf root@$ip:/usr/share/lightdm/lightdm.conf.d/ &
-done
