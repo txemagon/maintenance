cdefb16c108d2dce478d50a302eb930b64cc1cb3
txemagon <txema.gonz@gmail.com>
2015-11-05 09:41:55 +0100

    working on rcp

--- usr/share/nettenance/networking/local.sh
+++ usr/share/nettenance/networking/local.sh
@@ -1,6 +1,6 @@
 
 # find the ip your using to connect to $1
-my_ip () {
+my_ip_for () {
     ip -o route get $1 | \
     sed -n 's/.* src//p' - | \
     sed -n  's/\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}/\nip&\n/gp' - | \
--- usr/share/nettenance/networking/scheme.sh
+++ usr/share/nettenance/networking/scheme.sh
@@ -3,43 +3,14 @@
 # Parses an URL.
 
 parse-scheme() {
-    # extract the protocol
-    proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
-    # remove the protocol
-    url="$(echo ${1/$proto/})"
-    # extract the user (if any)
-    userpass="$(echo $url | grep @ | cut -d@ -f1)"
-    pass=`echo $userpass | grep : | cut -d: -f2`
-    if [ -n "$pass" ]; then
-        user=`echo $userpass | grep : | cut -d: -f1`
-    else
-        user=$userpass
+    local input=$1
+    if [ -z $1 ]; then
+        read -r input
     fi
-    # extract the host
-    hostport=`echo $url | sed -e s,$userpass@,,g | cut -d/ -f1`
-    port=`echo $hostport | grep : | cut -d: -f2`
-    if [ -n "$port" ]; then
-        host=`echo $hostport | grep : | cut -d: -f1`
-    else
-        host=$hostport
-    fi
-    # extract the path (if any)
-    path="$(echo $url | grep / | cut -d/ -f2-)"
-
-    echo "url: $url"
-    echo "  proto: $proto"
-    echo "  user: $user"
-    echo "  pass: $pass"
-    echo "  host: $host"
-    echo "  port: $port"
-    echo "  path: $path"
-}
-
-parse-scp-scheme() {
     # extract the protocol
-    proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    proto="$(echo $input | grep :// | sed -e's,^\(.*://\).*,\1,g')"
     # remove the protocol
-    url="$(echo ${1/$proto/})"
+    url="$(echo ${input/$proto/})"
     # extract the user (if any)
     userpass="$(echo $url | grep @ | cut -d@ -f1)"
     pass=`echo $userpass | grep : | cut -d: -f2`
@@ -66,5 +37,5 @@ parse-scp-scheme() {
     echo "  host: $host"
     echo "  port: $port"
     echo "  path: $path"
-
 }
+
